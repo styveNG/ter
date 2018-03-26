180f86767751cd72cc4372377d7039b0abfe034a
Styve <styvengoyi@yahoo.fr>
2018-03-26 18:52:16 +0200

    dataset

--- dataset.py
+++ dataset.py
@@ -1,22 +1,5 @@
-class Item:
-    def __init__(self,nom_item,spe_tech=None):
-        self.nom_item=nom_item
-        self.spe_tech=spe_tech
-    def __repr__(self):
-        return "{} ({})".format(self.nom_item, self.spe_tech)
-
-
-# Permet de creer les transactions
-class Transaction:
-    def __init__(self,nom_transac,lst_item):
-        self.nom_transac=nom_transac
-        self.lst_item=lst_item
-        maListe=[]
-        for item in lst_item:
-            maListe.append(Item(item))
-    def __repr__(self):
-        return "{} : {}".format(self.nom_transac,self.lst_item)
-
+from Items import Item
+from transaction import Transaction
 
 # Permet de creer le jeu de donnees TRANSACTION --> Items
 class Dataset:
