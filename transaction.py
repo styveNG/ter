7517497e8dd74457aa1e9c84169ad240df01332c
Styve <styvengoyi@yahoo.fr>
2018-03-27 22:29:47 +0200

    transaction

--- transaction.py
+++ transaction.py
@@ -1,6 +1,5 @@
 from Items import Item
 
-
 # Permet de creer les transactions
 class Transaction:
     def __init__(self,nom_transac):
@@ -12,11 +11,17 @@ class Transaction:
     def ajouterItem(self, unItem):
         self.lst_item.append(unItem)
 
+    #IDEE: Creer une methode qui rajoute les spetech
+    def ajouterSpetech(self):
+        self.lst_spetech=[]
+        pass
+
     #transforme chaque produit de la liste en item
     def ajouterListeProd(self, listeProd):
         for produit in listeProd:
             item = Item(produit)  #transformation du produit parcouru en item
             self.ajouterItem(item) #appel à la méthode ajouter item
+
 
     def __repr__(self):
         return "{} : {}".format(self.nom_transac,self.lst_item)
@@ -30,24 +35,21 @@ class Transaction:
 # PROGRAMME PRINCIPAL
 #####################
 
+#Creation des items
 pain = Item("pain", ["250g"])
 couches = Item("couches", ["20couches", "5euros"])
 
+#Creation d'une transaction avec des items crees precedement
 monCaddie1= Transaction("T1")
-monCaddie2 = Transaction("T2")
-
 monCaddie1.ajouterItem(pain)
 monCaddie1.ajouterItem(couches)
-
-monCaddie2.ajouterListeProd(["pain", "couches", "biere", "œufs"])
-
 #print(monCaddie1)
-#
+#print(monCaddie1.lst_item[0].spetech) : permet d'accéder à la spetech de l'item d'indice 0 du caddie 1
+
+#Creation d'une transaction a l'aide d'une liste de produit transformer en item
+monCaddie2 = Transaction("T2")
+monCaddie2.ajouterListeProd(["pain", "couches", "biere", "œufs"])
 #print(monCaddie2)
-# # affiche T2 : [pain: None, couches: None, biere: None, œufs: None] : c'est une liste qui contient des objets de type item
-# # c'est parce que dans le repr de item on lui a demandé d'afficher le nom et la spé tech qu'on a ce résultat*
-# #si on n'avait demandé dans ce repr de n'afficher que le nom, on n'aurait eu que le nom des items
-#
-# print(monCaddie2.lst_item[1].nom)   #permet d'accéder à l'item d'indice 1 du caddie 2
+print(monCaddie2.lst_item[1].nomItem)   #permet d'accéder à l'item d'indice 1 du caddie 2
 
 
