8cbe38ddd830c9b4888c71120f59d308e1851335
Styve <styvengoyi@yahoo.fr>
2018-03-28 10:41:14 +0200

    dataset

--- dataset.py
+++ dataset.py
@@ -1,20 +1,27 @@
 from Items import Item
 from transaction import Transaction
 
-# Permet de creer le jeu de donnees TRANSACTION --> Items
+# Permet de creer le jeu de donnees TRANSACTIONS --> Items
 class Dataset:
-    def __init__(self,lst_transactions):
-        self.lst_transactions=lst_transactions
+    def __init__(self):
+        self.lst_transactions=[]
+
+    def ajouterTransaction(self,maTransaction):
+        self.lst_transactions.append(maTransaction)
+
+    def __repr__(self):
+        return "{}".format(self.lst_transactions)
 
 
     #parcourir toutes transactions et pour chaque transaction parcourrir tous les items de le transaction
     # en faire un itemset et verifier si on l'a deja dans le liste des singleton
-
-
     # Identifie les singletons presents dans les transactions
     # Renvoie une liste d'itemset de taille 1 non redondant
     def singleton(self):
-        #creer une liste vide
+        lst_singleton=[]
+        for transac in self.lst_transactions:
+            print(transac)
+
         pass
 
     # cree une liste d'itemset frequent a partir d'une liste d'itemset et d'un minSup
@@ -31,8 +38,42 @@ class Dataset:
 # PROGRAMME PRINCIPAL
 #####################
 
+# Creation des items
+pain = Item("pain")
+lait = Item("lait", ["1L", "brique"])
+couches = Item("couches", ["20couches"])
+biere = Item("biere", ["50cL", "canette"])
+oeufs = Item("oeufs", ["6", "barquette"])
+coca = Item("coca", ["2L", "bouteille", "zero"])
+cake = Item("cake")
+
+# Creation des transactions
 monCaddie1= Transaction("T1")
+monCaddie1.ajouterItem(pain)
+monCaddie1.ajouterItem(couches)
+monCaddie1.ajouterItem(biere)
+
 monCaddie2 = Transaction("T2")
+monCaddie2.ajouterItem(couches)
+monCaddie2.ajouterItem(cake)
+monCaddie2.ajouterItem(coca)
+monCaddie2.ajouterItem(oeufs)
+
+monCaddie3 = Transaction("T3")
+monCaddie3.ajouterItem(lait)
+monCaddie3.ajouterItem(pain)
+monCaddie3.ajouterItem(oeufs)
+#### Fin des transactions ####
+
+# Creation du dataset
+monData=Dataset()
+monData.ajouterTransaction(monCaddie1)
+monData.ajouterTransaction(monCaddie2)
+monData.ajouterTransaction(monCaddie3)
+
+#print(monData)
+
+
 
 
 
