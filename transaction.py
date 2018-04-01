e6c8e0f77c0759bc18dd964355bc6a4c995c1be8
Styve <styvengoyi@yahoo.fr>
2018-04-02 01:41:52 +0200

    transaction

--- transaction.py
+++ transaction.py
@@ -1,4 +1,4 @@
-from Items import Item
+from items import Item
 
 # Permet de creer les transactions
 class Transaction:
@@ -11,7 +11,7 @@ class Transaction:
     def ajouterItem(self, unItem):
         self.lst_item.append(unItem)
 
-    #IDEE: Creer une methode qui rajoute les spetech
+    #IDEE: Creer une methode qui rajoute les spetech???
     def ajouterSpetech(self):
         self.lst_spetech=[]
         pass
@@ -35,21 +35,6 @@ class Transaction:
 # PROGRAMME PRINCIPAL
 #####################
 
-#Creation des items
-pain = Item("pain", ["250g"])
-couches = Item("couches", ["20couches", "5euros"])
 
-#Creation d'une transaction avec des items crees precedement
-monCaddie1= Transaction("T1")
-monCaddie1.ajouterItem(pain)
-monCaddie1.ajouterItem(couches)
-#print(monCaddie1)
-#print(monCaddie1.lst_item[0].spetech) : permet d'accéder à la spetech de l'item d'indice 0 du caddie 1
-
-#Creation d'une transaction a l'aide d'une liste de produit transformer en item
-monCaddie2 = Transaction("T2")
-monCaddie2.ajouterListeProd(["pain", "couches", "biere", "œufs"])
-#print(monCaddie2)
-#print(monCaddie2.lst_item[1].nomItem)   #permet d'accéder à l'item d'indice 1 du caddie 2
 
 
